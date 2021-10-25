b33d14a2ebba8024dfc2e90e6e5dcf23aa8350e6
Chris <nico.bar2012@gmail.com>
2021-10-26 01:33:59 +0200

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Test Docusaurus',
+  title: 'B3 Dev',
   tagline: 'Dinosaurs are cool, but coffee is better !',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
@@ -43,7 +43,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'B3 Dev',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -106,7 +106,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Cours Ynov. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
